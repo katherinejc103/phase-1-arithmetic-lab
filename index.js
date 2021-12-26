08a1906583c78e24fd1b620e8bba51328bc19516
Katherine Jean-Charles <katherinejc103@gmail.com>
2021-12-26 12:53:35 -0500

    commit tree 4e5f920f618d735ee3b3a0d0edc31bc167379c4d

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 
-// Write your code here
-function multiply () {
+// Question one
 let multiply, num1, num2;
 num1 = 31;
 num2 = 2;
 multiply = num1 * num2;
-}
+
+//Question Two
+let random;
+random = Math.ceil(Math.random()) + 1;
+
+//Question three
+let mod, num3, num4;
+num3 = 22;
+num4 = 6;
+mod = num3 % num4;
+
+
+// Question four
+let max;
+max = Math.max(1, 20, 15, 4, 9)
+
+
+
